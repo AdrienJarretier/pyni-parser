55d3ff835f021e890f1e0bb1e176977e5d6de78a
Adrien Jarretier-Yuste <jarretier.adrien@gmail.com>
2021-06-16 02:20:38 +0200

    parse a file line by line and detect sections from options

--- IniParser.py
+++ IniParser.py
@@ -2,3 +2,27 @@ class IniParser():
 
     def __init__(self):
         pass
+
+    def _parseLine(self, line):
+        print(line)
+        if line.startswith('['):
+            print('section')
+        else:
+            print('option')
+
+    # parse entire string from an ini file
+    def parseString(self, s):
+        i = 0
+        for l in s.split():
+            self._parseLine(l)
+            i += 1
+
+    def parseFile(self, filename):
+
+        with open(filename) as f:
+            self.parseString(f.read())
+
+
+parser = IniParser()
+
+parser.parseFile('tests/0-readAndOutput/input.ini')
